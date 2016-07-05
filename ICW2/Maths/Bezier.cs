865b33e8e99092b0f7c822208a598b1bb3b90cc8
Marie Mandrela <marie.h.mandrela@gmail.com>
2016-07-05 13:52:10 +0200

    Draw Bezier curve for given points.

---
+++ ICW2/Maths/Bezier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ICW2.Maths
+{
+    static class Bezier
+    {
+        public static PolyLineSegment GetBezierApproximation(Point[] controlPoints, int outputSegmentCount)
+        {
+            Point[] points = new Point[outputSegmentCount + 1];
+            for (int i = 0; i <= outputSegmentCount; i++)
+            {
+                double t = (double)i / outputSegmentCount;
+                points[i] = GetBezierPoint(t, controlPoints, 0, controlPoints.Length);
+            }
+
+            return new PolyLineSegment(points, true);
+        }
+
+        static Point GetBezierPoint(double t, Point[] controlPoints, int index, int count)
+        {
+            if (count == 1)
+            {
+                return controlPoints[index];
+            }
+
+            var p1 = GetBezierPoint(t, controlPoints, index, count - 1);
+            var p2 = GetBezierPoint(t, controlPoints, index + 1, count - 1);
+            return new Point((t - 1) * p1.X + t * p2.X, (1 - t) * p1.Y + t * p2.Y);
+        }
+    }
+}
--- ICW2/Program.cs
+++ ICW2/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
+using System.Windows.Media;
 using System.Drawing.Imaging;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ICW2.Maths;
+
+using MPoint = System.Windows.Point;
+using DColor = System.Drawing.Color;
 
 namespace ICW2
 {
@@ -12,15 +17,40 @@ namespace ICW2
     {
         static void Main(string[] args)
         {
-            String file = "../../Output/test.png";
-            Bitmap bm = new Bitmap(500, 500);
+            String file = "../../Output/bezier.png";
+            Bitmap bm = new Bitmap(1000, 1000);
 
-            fillRandom(bm, Color.Turquoise);
+            MPoint[] testPoints = new[] {
+                new MPoint(100, 50),
+                new MPoint(50, 200),
+                new MPoint(300, 100),
+                new MPoint(150, 100),
+                new MPoint(60, 400),
+                new MPoint(460, 160),
+                new MPoint(100, 50),
+            };
+            drawBezier(bm, testPoints, DColor.White, 250, 350, true);
 
             bm.Save(file, ImageFormat.Png);
         }
 
-        static void fillRandom(Bitmap bm, Color c)
+        static void drawBezier(Bitmap bm, MPoint[] points, DColor c, int xOffset = 0, int yOffset = 0, bool drawAnchors = false)
+        {
+            PolyLineSegment line = Bezier.GetBezierApproximation(points, 1000);
+
+            foreach (MPoint p in line.Points)
+            {
+                bm.SetPixel((int) p.X + xOffset, (int) p.Y + yOffset, c);
+            }
+
+            foreach (MPoint p in points)
+            {
+                bm.SetPixel((int)p.X + xOffset, (int)p.Y + yOffset, c);
+            }
+        }
+
+
+        static void drawRandom(Bitmap bm, DColor c)
         {
             Random rnd = new Random();
 
