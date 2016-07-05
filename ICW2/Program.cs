2b18efe4296bc39af1bf69711f244968f6d0d511
Marie Mandrela <marie.h.mandrela@gmail.com>
2016-07-05 11:54:54 +0200

    Initial Commit. Random Image Generation.

---
+++ ICW2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String file = "../../Output/test.png";
+            Bitmap bm = new Bitmap(500, 500);
+
+            fillRandom(bm, Color.Turquoise);
+
+            bm.Save(file, ImageFormat.Png);
+        }
+
+        static void fillRandom(Bitmap bm, Color c)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < bm.Width; i++)
+            {
+                for (int j = 0; j < bm.Height; j++)
+                {
+                    if (rnd.NextDouble() > 0.5)
+                    {
+                        bm.SetPixel(i, j, c);
+                    }
+                }
+            }
+        }
+    }
+}
